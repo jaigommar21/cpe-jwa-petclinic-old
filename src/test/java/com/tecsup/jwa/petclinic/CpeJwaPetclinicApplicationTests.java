0ebda1187190e93217d50ad74435ea219169ab40
mbtec22 <mbtec22@192.168.1.4>
2020-07-23 15:31:52 -0500

    first commit

---
+++ src/test/java/com/tecsup/jwa/petclinic/CpeJwaPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.jwa.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpeJwaPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
